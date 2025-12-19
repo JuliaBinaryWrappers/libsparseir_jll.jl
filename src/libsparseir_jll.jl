3e95fe23106bbfb462358fa2dafa0d1131cebeed
jlbuild <juliabuildbot@gmail.com>
2025-12-19 18:09:53 +0000

    libsparseir_jll build 0.7.3+0

--- src/libsparseir_jll.jl
+++ src/libsparseir_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libsparseir")
-JLLWrappers.@generate_main_file("libsparseir", UUID("e5bf0e3f-5e75-5bb8-9f3f-2c07620468a3"))
+JLLWrappers.@generate_main_file("libsparseir", Base.UUID("e5bf0e3f-5e75-5bb8-9f3f-2c07620468a3"))
 end  # module libsparseir_jll
